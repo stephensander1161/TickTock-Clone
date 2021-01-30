3cf466502589481660f26ac0bdfb7a1f5b5d362d
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-01-29 19:47:28 -0700

    deployed backend
    moved to separate repo

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
 import Video from './Video';
-import day8 from './media/Day 8.mp4';
+import axios from './axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+	const [ videos, setVideos ] = useState([]);
+
+	useEffect(() => {
+		async function fetchPosts() {
+			const response = await axios.get('./v2/posts');
+			setVideos(response.data);
+
+			return response;
+		}
+
+		fetchPosts();
+	}, []);
 	return (
 		<div className="app">
-			<h1>Hi Moms</h1>
-
 			<div className="app__videos">
-				<Video
-					url={day8}
-					channel="sandman"
-					description="come hang with the man of sand"
-					song="will you love me"
-					likes={123}
-					messages={456}
-					shares={234}
-				/>
-				<Video />
+				{videos.map(({ url, channel, description, song, likes, messages, shares }) => (
+					<Video
+						url={url}
+						channel={channel}
+						song={song}
+						likes={likes}
+						messages={messages}
+						description={description}
+						shares={shares}
+					/>
+				))};
 			</div>
 		</div>
 	);
--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react';
 import './Video.css';
 import VideoFooter from './VideoFooter';
 import VideoSideBar from './VideoSideBar';
+import ReactPlayer from 'react-player';
 
 function Video({ url, channel, description, song, likes, messages, shares }) {
 	const [ playing, setPlaying ] = useState(false);
@@ -18,7 +19,7 @@ function Video({ url, channel, description, song, likes, messages, shares }) {
 	};
 	return (
 		<div className="video">
-			<video onClick={handleVideoPress} className="video__player" loop ref={videoRef} src={url} />
+			<ReactPlayer onClick={handleVideoPress} className="video__player" loop ref={videoRef} url={url} />
 
 			<VideoFooter channel={channel} description={description} song={song} />
 			<VideoSideBar likes={likes} shares={shares} messages={messages} />
